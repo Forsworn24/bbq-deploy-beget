32da9fae5c9da97972d5fc1613713fbc98eb8bae
Forsworn24 <mudinov.ali2401@gmail.com>
2022-10-03 20:53:08 +0500

    change app name capitstano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "www"
+set :application, "bbq"
 # set :repo_url, "git@example.com:me/my_repo.git"
 
 # Это имя приложения
