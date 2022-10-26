6f4a1ecd130007047e0465f35808713df97b6763
Forsworn24 <mudinov.ali2401@gmail.com>
2022-10-26 17:14:47 +0500

    del add image

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,7 +275,7 @@ Devise.setup do |config|
                   Rails.application.credentials.dig(:omniauth, :github, :secret), scope: 'user,public_repo'
 
   config.omniauth :google_oauth2, Rails.application.credentials.dig(:omniauth, :google, :id),
-                  Rails.application.credentials.dig(:omniauth, :google, :secret)
+                  Rails.application.credentials.dig(:omniauth, :google, :secret), scope: 'profile'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
