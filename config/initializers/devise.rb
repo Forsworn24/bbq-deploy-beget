d68f34c4bbcd8345668dfd16ab86dc874ee89166
Forsworn24 <mudinov.ali2401@gmail.com>
2022-10-26 16:36:52 +0500

    test oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,10 +272,11 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :github, Rails.application.credentials.dig(:omniauth, :github, :id),
-                  Rails.application.credentials.dig(:omniauth, :github, :secret), scope: 'user,repo,gist'
+                  Rails.application.credentials.dig(:omniauth, :github, :secret), scope: 'user,public_repo'
 
   config.omniauth :google_oauth2, Rails.application.credentials.dig(:omniauth, :google, :id),
                   Rails.application.credentials.dig(:omniauth, :google, :secret)
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
