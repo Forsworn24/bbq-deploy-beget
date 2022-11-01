eb7958933ba33edfcad1715a1797e33d9e1d2322
Forsworn24 <mudinov.ali2401@gmail.com>
2022-11-02 00:53:07 +0500

    add github.rb

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,4 @@
-use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, Rails.application.credentials.dig(:omniauth, :github, :id), 
     Rails.application.credentials.dig(:omniauth, :github, :secret), scope: 'read:user, user:email'
 end
